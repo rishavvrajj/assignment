86cda2817a746b76b11600a3ca05d82a0d6834ff
Rishav Raj <rishav5raj3@example.com>
2026-06-18 19:05:17 +0530

    data format fix

--- class-8.2/script.js
+++ class-8.2/script.js
@@ -3,12 +3,12 @@ const axios = require('axios');
 async function main() {
     const response = await fetch('https://fake-json-api.mock.beeceptor.com/users');
     const json = await response.json();
-    console.log('Fetch: ' + json.length)    
+    console.log('Fetch: ' + JSON.stringify(json))
 }
 
 async function axiosMain() {
     const response = await axios.get('https://fake-json-api.mock.beeceptor.com/users')
-    console.log('Axios: ' + response.data.length)
+    console.log('Axios: ' + JSON.stringify(response.data))
 }
 
 main()
